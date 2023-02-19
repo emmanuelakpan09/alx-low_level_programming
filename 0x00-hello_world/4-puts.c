05e50daa0606ab8615fa84518e9ae7ed3dbddbc1
emmanuelakpan09 <emmanuelakpan09@gmail.com>
2023-02-19 06:17:21 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - programming is a fun puzzle
+ *
+ *return: 0 (sucess)
+ */
+
+int main(void)
+{
+puts("\"Programming is like a multilingual puzzle");
+
+return (0);
+}
