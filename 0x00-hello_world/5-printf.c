59ab8ae55a9a11a9e2e5a512681e196550740e75
emmanuelakpan09 <emmanuelakpan09@gmail.com>
2023-02-19 06:37:49 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - printing proper grammar with c programming
+ *return: 0
+ */
+
+int main(void)
+}
+printf("with proper grammar, but the outcome is piece of art,\n");
+return (0);
+}
