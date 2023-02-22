4fa469a900e36d1ce28c3ad84da13db3cfd37ed6
emmanuelakpan09 <emmanuelakpan09@gmail.com>
2023-02-22 08:56:11 -0800

    0 putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
 #include "main.h"
 /**
-* _isupper - checks for uppercase character
-* @c: Variable text
-* Return: Always 0.
+* main - print _putchar
+* is written by mbah
+* Return: 0
 */
-int _isupper(int c)
+int main(void)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
-}
+
 
